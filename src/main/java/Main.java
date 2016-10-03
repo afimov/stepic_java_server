987a4ac331fc15cccaf74d7c70b393818f17afaa
Anton <anton@MacBook-Pro-Anton.local>
2016-10-03 21:21:21 +0300

    Simple web server using jetty library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,23 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
+
 /**
  * Created by anton on 03.10.16.
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new AllRequestsServlet()),"/*");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
     }
 }
